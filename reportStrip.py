7af3de6d0f055bce2dd557687099d2fabf76cf5b
spaceB0x <welton.tyler@gmail.com>
2014-02-12 08:29:12 -0600

    reportStrip.py --Updated Main File
    Updated main file

--- reportStrip.py
+++ reportStrip.py
@@ -18,6 +18,7 @@
 
 import os
 import optparse
+import re
 
 def main():
 
@@ -39,17 +40,63 @@ def main():
 	f = ("%s.txt" % str(options.output)) #append '.txt.'
 	wfile = open(f,'w')
 	rfile= options.filename 
+	print 'Parsing.....'
 	os.system('python pdf2txt.py -o temp.txt "%s"' %rfile)
-	
 	
 	temp = open('temp.txt','rb')
 	
 	###STRIP###
-	print 'Parsing.....'
+	
+	wfile.write('Suspicious Subject lines: \n')             #write Subject list
 	for line in temp:
   		if (line.find('Subject:') != -1):
-			wfile.write(line.lstrip("Subject:").replace('!', ' ') +"\n")
+				wfile.write(line.lstrip("Subject:").replace('!', ' '))			
+	temp.close()
+	temp = open('temp.txt', 'rb')
+							
+	wfile.write('\n\n\n	Formatted for Mimecast:\n')				#write formatted list for mimecast
+	fmtString = ''
+	for line in temp:
+		if (line.find('Subject:') != -1):
+				fmtString=(fmtString +'"'+(line.lstrip("Subject:").lstrip('\n').lstrip('\r').replace('!',' ')) +'"'+' OR ')
+	wfile.write('%s' % fmtString)
+	temp.close()
+	temp = open('temp.txt', 'rb')
+
+	wfile.write('\n\n\n')									#print IPs using regex
+	wfile.write('Suspicious IP addresses: \n')
+	for line in temp:
+		foundIPs = re.findall(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}', line)
+		if foundIPs:
+			for ip in foundIPs:
+				wfile.write('%s\n' %ip)
+	temp.close()
+	temp = open('temp.txt', 'rb')
+				
+	wfile.write('\n\n\n')									#write formatted IPs for SW/Splunk
+	wfile.write('IPs Formatted for Splunk: \n')
+	for line in temp:
+		foundIPs = re.findall(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}', line)
+		if foundIPs:
+			for ip in foundIPs:
+				wfile.write('%s\n' %ip)
+	
 		
+	wfile.write('\n\n\n')									#print Suspect domains.
+	wfile.write('Suspicious Domains: \n')
+	
+	temp.close()
+	temp = open('temp.txt', 'rb')
+	
+	for line in temp:
+		domains = re.findall(r'\@?[\w.]+.com',line)
+		if domains:
+			for d in domains:
+				d=d.replace('!','.')
+				wfile.write('%s\n' %d)
+		
+	wfile.write('\n\n\n')									#write formatted for Mimecast
+	wfile.write('Domains formatted for Mimecast: \n')		
 	
 	print '\n DONE!'
 	temp.close()
